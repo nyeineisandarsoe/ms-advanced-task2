250e2d6cf7178e6fcfff766b5cb7f2b50f393877
Sandar <Nyein_Ei_Sandar_Soe_from.tp@simtech.a-star.edu.sg>
2022-07-22 22:16:04 +0800

    first commit

---
+++ ConsumerService.cs
@@ -0,0 +1,109 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using System.Text.Json;
+using TaskProcessor.Model;
+using System.Threading.Tasks;
+
+namespace TaskProcessor
+{
+    public class ConsumerService : BackgroundService
+
+    {
+        private readonly ILogger _logger;
+        private IConnection _connection;
+        private IModel _channel;
+        private TaskContext _context;
+
+        public ConsumerService(ILoggerFactory loggerFactory)
+        {
+            this._logger = loggerFactory.CreateLogger<ConsumerService>();
+            InitRabbitMQ();
+        }
+
+        private void InitRabbitMQ()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST"),
+                Port = Convert.ToInt32(Environment.GetEnvironmentVariable("RABBITMQ_PORT"))
+
+            };
+
+            // create connection  
+            _connection = factory.CreateConnection();
+
+            // create channel  
+            _channel = _connection.CreateModel();
+
+            _channel.QueueDeclare("tasks", false, false, false, null);
+
+            _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                // received message  
+                var content = System.Text.Encoding.UTF8.GetString(ea.Body.ToArray());
+
+                // handle the received message  
+                HandleMessageAsync(content);
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            consumer.Shutdown += OnConsumerShutdown;
+            consumer.Registered += OnConsumerRegistered;
+            consumer.Unregistered += OnConsumerUnregistered;
+            consumer.ConsumerCancelled += OnConsumerConsumerCancelled;
+
+            _channel.BasicConsume("greetings", false, consumer);
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleMessageAsync(string content)
+        {
+            // we just print this message   
+            _logger.LogInformation($"consumer received {content}");
+            Console.WriteLine($"consumer received {content}");
+
+            var obj = JsonSerializer.Deserialize<TaskModel>(content);
+
+            _context.Tasks.Add(obj);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        private void OnConsumerConsumerCancelled(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerUnregistered(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerRegistered(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerShutdown(object sender, ShutdownEventArgs e) { }
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e) { }
+
+        public override void Dispose()
+        {
+            _channel.Close();
+            _connection.Close();
+            base.Dispose();
+        }
+
+    }
+}
